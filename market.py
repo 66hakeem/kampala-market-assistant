a66ec6ee42812d56adb113d18f2032e07e526e84
Hakeem Matovu <66hakeem@gmail.com>
2025-08-15 23:01:46 +0300

    feat: Checks if item is in stock

--- market.py
+++ market.py
@@ -1,19 +1,19 @@
 produce = {
     "mango" : {
         "price": 2500,
-        "stock": 15
+        "stock": 2
     },
     "orange": {
         "price": 2000,
-        "stock": 20
+        "stock": 0
     },
     "banana": {
         "price": 1500,
-        "stock": 10
+        "stock": 13
     }
 }
 
-shopping_list = ["mango", "banana", "pear"]
+shopping_list = ["mango", "banana", "pear", "orange"]
 
 def calculate_bill(produce_items, customer_list):
     
@@ -21,13 +21,16 @@ def calculate_bill(produce_items, customer_list):
     
     for item in customer_list:
         if item in produce_items:
-            print(f"Adding {item} to the cart.")
-            total_cost = total_cost + produce_items[item]["price"]
+            if produce_items[item]["stock"] == 0:
+                print(f"Sorry, {item} is currently out of stock")
+            else:
+                print(f"Adding {item} to the cart.")
+                total_cost = total_cost + produce_items[item]["price"]
         else:
             print(f"Sorry, we are out of stock for {item}.")
     return total_cost
 
 final_bill = calculate_bill(produce, shopping_list)
 
-print(f"Your total bill is {final_bill}")
+print(f"Your total bill is {final_bill}.")
         
